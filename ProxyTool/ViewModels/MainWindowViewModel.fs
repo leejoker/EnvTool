877b49ddee4924507522c4d39c5e98bc18a5e1e3
leejoker <1056650571@qq.com>
2024-01-22 22:07:50 +0800

    back button available

--- ProxyTool/ViewModels/MainWindowViewModel.fs
+++ ProxyTool/ViewModels/MainWindowViewModel.fs
@@ -5,13 +5,20 @@ open ReactiveUI
 type MainWindowViewModel() as this =
     inherit ViewModelBase()
 
+    let _mainView = new MainViewModel()
+
     let mutable contentViewModel: ViewModelBase = Unchecked.defaultof<ViewModelBase>
 
-    do this.ContentViewModel <- new MainViewModel()
+    do this.ContentViewModel <- _mainView
 
     member this.ContentViewModel
         with get () = contentViewModel
         and private set (value: ViewModelBase) = this.RaiseAndSetIfChanged(&contentViewModel, value) |> ignore
 
     member this.ProxyConfig() =
-        this.ContentViewModel <- new ProxyConfigViewModel()
+        let proxyConfigModel = new ProxyConfigViewModel()
+
+        proxyConfigModel.BackCommand.Subscribe(fun _ -> this.ContentViewModel <- _mainView)
+        |> ignore
+
+        this.ContentViewModel <- proxyConfigModel
--- ProxyTool/ViewModels/ProxyConfigViewModel.fs
+++ ProxyTool/ViewModels/ProxyConfigViewModel.fs
@@ -1,4 +1,16 @@
 ﻿namespace ProxyTool.ViewModels
 
-type ProxyConfigViewModel() =
+open ReactiveUI
+open System
+
+type ProxyConfigViewModel() as this =
     inherit ViewModelBase()
+
+    let mutable backCommand =
+        Unchecked.defaultof<ReactiveCommand<Reactive.Unit, Reactive.Unit>>
+
+    do this.BackCommand <- ReactiveCommand.Create(fun () -> ())
+
+    member this.BackCommand
+        with get () = backCommand
+        and private set v = backCommand <- v
