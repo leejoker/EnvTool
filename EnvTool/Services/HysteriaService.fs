3229af2e4f0676a0e1924979f410e3c1d621ecd9
leejoker <1056650571@qq.com>
2024-09-18 18:39:13 +0800

    add HysteriaService.fs

---
+++ EnvTool/Services/HysteriaService.fs
@@ -0,0 +1,35 @@
+﻿namespace EnvTool.Services
+
+open System.Diagnostics
+
+module HysteriaService =
+
+    type ProcessStatusTuple = bool * Option<Process>
+
+    let StartHysteriaProcess (exec: string) (config: string) : ProcessStatusTuple =
+        let p = new Process()
+        p.StartInfo.FileName <- $"\"{exec}\""
+        p.StartInfo.Arguments <- $"-c \"{config}\""
+        p.StartInfo.UseShellExecute <- false
+        p.StartInfo.RedirectStandardInput <- true
+        p.StartInfo.RedirectStandardOutput <- true
+        p.StartInfo.RedirectStandardError <- true
+        p.StartInfo.CreateNoWindow <- true
+        p.Start() |> ignore
+
+        if p.WaitForExit(1000) then
+            p.CancelErrorRead()
+            (false, None)
+        else
+            (true, Some(p))
+
+    let KillHysteriaProcess (proc: Option<Process>) =
+        match proc with
+        | Some(p) ->
+            p.Kill()
+            p.WaitForExit(100) |> ignore
+
+            if p.HasExited |> not then
+                p.Kill()
+                p.WaitForExit(100) |> ignore
+        | None -> ()
