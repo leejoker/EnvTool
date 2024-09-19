b88c4425ab016b218b9808ca3e790196759fb416
leejoker <1056650571@qq.com>
2024-09-19 15:26:26 +0800

    add default proxyIgnore

--- EnvTool/Utils/ProxyUtils.fs
+++ EnvTool/Utils/ProxyUtils.fs
@@ -6,6 +6,9 @@ open Microsoft.Win32
 #endif
 
 module ProxyUtils =
+    
+    let defaultWindowsProxyIgnore = "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*;127.0.0.1;<local>"
+    
     let GitProxyEnabled () : bool =
         let gitProxy = RunCmdCommand "git config --global --list | findstr proxy"
         gitProxy.Contains "http.proxy" || gitProxy.Contains "https.proxy"
@@ -61,7 +64,7 @@ module ProxyUtils =
 
         internetSettings.SetValue("ProxyEnable", 1)
         internetSettings.SetValue("ProxyServer", $"{host}:{port}")
-    // TODO 增加忽略
+        internetSettings.SetValue("ProxyOverride", defaultWindowsProxyIgnore)
 #endif
 #if Linux
 #endif
