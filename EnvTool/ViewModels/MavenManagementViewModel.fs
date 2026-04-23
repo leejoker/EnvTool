618d2cca160b3251e90523a73503eb1654ef1017
leejoker <1056650571@qq.com>
2026-04-23 18:59:35 +0800

    feat: add AddSource dialog properties and commands to MavenManagementViewModel

--- EnvTool/ViewModels/MavenManagementViewModel.fs
+++ EnvTool/ViewModels/MavenManagementViewModel.fs
@@ -5,6 +5,7 @@ open System.Collections.ObjectModel
 open System.Windows.Input
 open EnvTool.Services
 open ReactiveUI
+open System.Reactive.Linq
 
 type MavenSourceItem = {
     Id: string
@@ -17,15 +18,32 @@ type MavenManagementViewModel() as this =
     inherit ViewModelBase()
 
     let mutable currentVersion = ""
-    let mutable mavenHome = ""
     let mutable settingsContent = ""
     let mutable selectedSource: MavenSourceItem option = None
+    let mutable selectedTabIndex = 0
     let sources = ObservableCollection<MavenSourceItem>()
+
+    let mutable isAddDialogOpen = false
+    let mutable newSourceId = ""
+    let mutable newSourceName = ""
+    let mutable newSourceUrl = ""
+
+    let showAddDialogCmd = ReactiveCommand.Create(Action(this.ShowAddDialog))
+    let addSourceCmd = ReactiveCommand.Create(Action(this.AddSourceFromDialog))
+    let cancelAddDialogCmd = ReactiveCommand.Create(Action(this.CancelAddDialog))
 
     let loadDataCmd = ReactiveCommand.Create(Action(this.LoadData))
     let saveSettingsCmd = ReactiveCommand.Create(Action(this.SaveSettings))
     let removeSourceCmd = ReactiveCommand.Create(Action(this.RemoveSource))
     let setDefaultSourceCmd = ReactiveCommand.Create(Action(this.SetDefaultSource))
+
+    do
+        this.WhenAnyValue(fun (x: MavenManagementViewModel) -> x.SelectedTabIndex)
+        |> Observable.filter (fun idx -> idx = 1)
+        |> Observable.subscribe (fun _ -> this.LoadSettingsContent())
+        |> ignore
+
+        this.LoadData()
 
     member this.LoadDataCommand: ICommand = loadDataCmd
     member this.SaveSettingsCommand: ICommand = saveSettingsCmd
@@ -36,10 +54,6 @@ type MavenManagementViewModel() as this =
         with get () = currentVersion
         and set v = this.RaiseAndSetIfChanged(&currentVersion, v) |> ignore
 
-    member this.MavenHome
-        with get () = mavenHome
-        and set v = this.RaiseAndSetIfChanged(&mavenHome, v) |> ignore
-
     member this.SettingsContent
         with get () = settingsContent
         and set v = this.RaiseAndSetIfChanged(&settingsContent, v) |> ignore
@@ -48,7 +62,31 @@ type MavenManagementViewModel() as this =
         with get () = selectedSource
         and set v = this.RaiseAndSetIfChanged(&selectedSource, v) |> ignore
 
+    member this.SelectedTabIndex
+        with get () = selectedTabIndex
+        and set v = this.RaiseAndSetIfChanged(&selectedTabIndex, v) |> ignore
+
     member this.Sources: ObservableCollection<MavenSourceItem> = sources
+
+    member this.IsAddDialogOpen
+        with get () = isAddDialogOpen
+        and set v = this.RaiseAndSetIfChanged(&isAddDialogOpen, v) |> ignore
+
+    member this.NewSourceId
+        with get () = newSourceId
+        and set v = this.RaiseAndSetIfChanged(&newSourceId, v) |> ignore
+
+    member this.NewSourceName
+        with get () = newSourceName
+        and set v = this.RaiseAndSetIfChanged(&newSourceName, v) |> ignore
+
+    member this.NewSourceUrl
+        with get () = newSourceUrl
+        and set v = this.RaiseAndSetIfChanged(&newSourceUrl, v) |> ignore
+
+    member this.ShowAddDialogCommand: ICommand = showAddDialogCmd
+    member this.AddSourceCommand: ICommand = addSourceCmd
+    member this.CancelAddDialogCommand: ICommand = cancelAddDialogCmd
 
     member this.LoadData() =
         this.CurrentVersion <-
@@ -56,21 +94,17 @@ type MavenManagementViewModel() as this =
             | Some(v) -> v
             | None -> "Not found"
 
-        this.MavenHome <-
-            match MavenService.GetMavenHome() with
-            | Some(h) -> h
-            | None -> "Not configured"
-
-        this.SettingsContent <-
-            match MavenService.GetSettingsContent() with
-            | Some(c) -> c
-            | None -> ""
-
         sources.Clear()
         MavenService.GetSources()
         |> List.iter (fun (s: MavenSource) ->
             let item = { Id = s.Id; Name = s.Name; Url = s.Url; IsDefault = s.IsDefault }
             sources.Add(item))
+
+    member this.LoadSettingsContent() =
+        this.SettingsContent <-
+            match MavenService.GetSettingsContent() with
+            | Some(c) -> c
+            | None -> ""
 
     member this.SaveSettings() =
         MavenService.SaveSettings(this.SettingsContent)
@@ -107,3 +141,21 @@ type MavenManagementViewModel() as this =
                 |> List.iter (fun (s: MavenSource) ->
                     let item = { Id = s.Id; Name = s.Name; Url = s.Url; IsDefault = s.IsDefault }
                     sources.Add(item))
+
+    member this.ShowAddDialog() =
+        this.IsAddDialogOpen <- true
+
+    member this.CancelAddDialog() =
+        this.IsAddDialogOpen <- false
+        this.NewSourceId <- ""
+        this.NewSourceName <- ""
+        this.NewSourceUrl <- ""
+
+    member this.AddSourceFromDialog() =
+        if not (String.IsNullOrWhiteSpace(this.NewSourceId)) &&
+           not (String.IsNullOrWhiteSpace(this.NewSourceUrl)) then
+            this.AddSource(this.NewSourceId, this.NewSourceName, this.NewSourceUrl)
+            this.IsAddDialogOpen <- false
+            this.NewSourceId <- ""
+            this.NewSourceName <- ""
+            this.NewSourceUrl <- ""
